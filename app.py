8258f67d0f58517e3b6f079b51c64c5a1fc9a5b5
lgaeun <gaeungaeun4837@gmail.com>
2021-03-29 10:08:44 +0900

    ssl서버 구축

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, send_from_directory
+import ssl;
 
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/')
+def main():
+  return "Hello page"
+
+# @app.route('/', defaults={'path': ''})
+@app.route('/index/<path:path>')
 def web(path):
     return render_template('index.html');
 
+@app.route('/host')
+def host():
+  return render_template('host.html')
+
+@app.route('/guest')
+def guest():
+  return render_template('guest.html')
+
+
 if __name__ == "__main__":
-  # app.run(debug=True)
-  app.run(host="127.0.0.1", port="5000", debug=True)
+  app.run(host="127.0.0.1", port="5000", debug=True, ssl_context=(
+      '/Users/gaeun/ssl/localhost.crt', '/Users/gaeun/ssl/localhost.key'))
