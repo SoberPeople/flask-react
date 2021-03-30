6421b8b2fe697115c812d82f0ede422d7a4ab88c
lgaeun <gaeungaeun4837@gmail.com>
2021-03-30 13:55:52 +0900

    라우팅 정리

--- app.py
+++ app.py
@@ -8,18 +8,15 @@ def main():
   return "Hello page"
 
 # @app.route('/', defaults={'path': ''})
-@app.route('/index/<path:path>')
+@app.route('/<path:path>')
 def web(path):
+  if(path == 'first' or path == 'second') :
     return render_template('index.html');
-
-@app.route('/host')
-def host():
-  return render_template('host.html')
-
-@app.route('/guest')
-def guest():
-  return render_template('guest.html')
-
+  elif(path == 'host'):
+    return render_template('host.html');
+  elif(path == 'guest'):
+    return render_template('guest.html');
+    
 
 if __name__ == "__main__":
   app.run(host="127.0.0.1", port="5000", debug=True, ssl_context=(
