53d37406ebc6d85d57ad522da31d4ef6d3f8aea0
unknown <y29@naver.com>
2021-04-13 17:38:11 +0900

    ip주소 수정

--- app.py
+++ app.py
@@ -99,5 +99,5 @@ def web(path):
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port="5000", debug=True, ssl_context=(
+    app.run(host="0.0.0.0", port="5000", debug=True, ssl_context=(
         './ssl/localhost.crt', './ssl/localhost.key'))
