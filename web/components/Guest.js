b91eacc3c639bc08cae026203c369ff7903e9e46
unknown <y29@naver.com>
2021-04-23 14:36:00 +0900

    script tag 추가

--- web/components/Guest.js
+++ web/components/Guest.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import ScriptTag from "react-script-tag";
 
-export default class Guest extends Component {
-  render() {
-    return <div>Hello Guest</div>;
-  }
+function Guest() {
+  return (
+    <div>
+      <h1>Hello Guest!!!</h1>
+      <ScriptTag
+        type="text/javascript"
+        src="https://unpkg.com/peerjs@1.3.1/dist/peerjs.min.js"
+      />
+      <ScriptTag src="https://unpkg.com/axios/dist/axios.min.js" />
+      <h1>END</h1>
+    </div>
+  );
 }
+
+export default Guest;
