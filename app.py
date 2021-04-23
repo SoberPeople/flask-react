c10be0f1951cec48818d36e3d794a5e1c054ded2
dev0529 <xyzkim0529@gmail.com>
2021-04-23 15:02:31 +0900

    capture

--- app.py
+++ app.py
@@ -11,6 +11,22 @@ from gaze_tracking import GazeTracking
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def web():
+    return render_template('index.html')
+
+
+@app.route('/test/host')
+def host():
+    return render_template('host.html')
+
+
+@app.route('/test/guest')
+def guest():
+    return render_template('guest.html')
+
+
 network = 'normal'
 size = 416
 confidence = 0.25
@@ -26,108 +42,119 @@ gaze = GazeTracking()
 
 if network == "normal":
     print("loading yolo...")
-    yolo = YOLO("models/cross-hands.cfg", "models/cross-hands.weights", ["hand"])
+    yolo = YOLO("models/cross-hands.cfg",
+                "models/cross-hands.weights", ["hand"])
 elif network == "prn":
     print("loading yolo-tiny-prn...")
-    yolo = YOLO("models/cross-hands-tiny-prn.cfg", "models/cross-hands-tiny-prn.weights", ["hand"])
+    yolo = YOLO("models/cross-hands-tiny-prn.cfg",
+                "models/cross-hands-tiny-prn.weights", ["hand"])
 elif network == "v4-tiny":
     print("loading yolov4-tiny-prn...")
-    yolo = YOLO("models/cross-hands-yolov4-tiny.cfg", "models/cross-hands-yolov4-tiny.weights", ["hand"])
+    yolo = YOLO("models/cross-hands-yolov4-tiny.cfg",
+                "models/cross-hands-yolov4-tiny.weights", ["hand"])
 else:
     print("loading yolo-tiny...")
-    yolo = YOLO("models/cross-hands-tiny.cfg", "models/cross-hands-tiny.weights", ["hand"])
+    yolo = YOLO("models/cross-hands-tiny.cfg",
+                "models/cross-hands-tiny.weights", ["hand"])
 
 yolo.confidence = float(confidence)
 
 
 @app.route('/api/detection/', methods=['GET', 'POST'])
 def detection():
-    mat = request.form.get('file')
-    #opencv에서 읽기 위해 8비트 애들을 아스키로 변환
-    img_data = np.frombuffer(base64.b64decode(mat.replace('data:image/png;base64,','')), np.uint8) 
-    mat = cv2.imdecode(img_data,cv2.IMREAD_ANYCOLOR)
+    frame = request.form.get('file')
+    # opencv에서 읽기 위해 8비트 애들을 아스키로 변환
+    img_data = np.frombuffer(base64.b64decode(
+        frame.replace('data:image/png;base64,', '')), np.uint8)
+    frame = cv2.imdecode(img_data, cv2.IMREAD_ANYCOLOR)
 
-    if (request.method == 'POST'):        
-        
-        # print(mat[:30])
-        # print(request.form)
-        # img = cv2.imread(mat)
-        # cv2.imwrite('yo.png',img)
-        # print(mat)
-        
-        # cv2.imwrite('mm.png', mat) 
-        
-        width, height, inference_time, results = yolo.inference(mat)
-                
+    if (request.method == 'POST'):
+        userId = request.form.get('id')
+        print(userId)
+
+        cheat = False # 손 또는 눈 detect 여부
+
+        # 손 detect
+        width, height, inference_time, results = yolo.inference(frame)
+
         print("%s seconds: %s classes found!" %
-            (round(inference_time, 2), len(results)))
+              (round(inference_time, 2), len(results)))
 
-        ## 여기에 사진 저장(값 0이면 캡쳐)
-        if len(results) < 1: 
-            return json.dumps({"nums_of_hand": 0})
-
-        for detection in results:
-            id, name, confidence, x, y, w, h = detection
-
-            # draw a bounding box rectangle and label on the image
-            color = (255, 0, 255)
-            cv2.rectangle(mat, (x, y), (x + w, y + h), color, 1)
-            text = "%s (%s)" % (name, round(confidence, 2))
-            cv2.putText(mat, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.25, color, 1)
-
-            print("%s with %s confidence" % (name, round(confidence, 2)))
-                
+        # 손 0개 탐지
+        if len(results) < 1:
+            cheat = True
 
         ### gaze_Tracking ###
-        # while True:
-        # We get a new frame from the webcam
-        # _, 
-        frame = mat
+        # frame = mat
 
         # We send this frame to GazeTracking to analyze it
         gaze.refresh(frame)
 
-        frame = gaze.annotated_frame()
-        text = ""
+        frame = gaze.annotated_frame() # 동공 십자가 표시
+        eye_text = ""
 
         if gaze.is_right():
-            text = "Looking right"
+            cheat = True
+            eye_text = "Looking right"
         elif gaze.is_left():
-            text = "Looking left"
-        elif gaze.is_center():
-            text = "Looking center"
+            cheat = True
+            eye_text = "Looking left"        
         elif gaze.is_up():
-            text = "Looking up"
+            cheat = True
+            eye_text = "Looking up"
+        elif gaze.is_center():
+            cheat = False
+            eye_text = "Looking center"
+        else :
+            cheat = True
 
-        cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+        if cheat: #손 또는 눈이 안 보일 때
+            for detection in results: #손 detect가 있으면 그림그리기
+                id, name, confidence, x, y, w, h = detection
 
-        left_pupil = gaze.pupil_left_coords()
-        right_pupil = gaze.pupil_right_coords()
-        cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-        cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+                # draw a bounding box rectangle and label on the image
+                color = (255, 0, 255)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 1)
+                text = "%s (%s)" % (name, round(confidence, 2))
+                cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                            0.25, color, 1)
 
-        # if cv2.waitKey(1) == 27:
-        #     break
+                print("%s with %s confidence" % (name, round(confidence, 2)))
+        
+            cv2.putText(frame, eye_text, (90, 60),
+                    cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)  # 눈 text쓰기
 
-        output_path = os.path.join(output_dir, str(uuid.uuid4()) + ".jpg")        
-        cv2.imwrite(output_path, frame)
-        return json.dumps({"nums_of_hand": len(results), "output_path": output_path})
+            left_pupil = gaze.pupil_left_coords()
+            right_pupil = gaze.pupil_right_coords()
+            cv2.putText(frame, "Left pupil:  " + str(left_pupil),
+                        (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+            cv2.putText(frame, "Right pupil: " + str(right_pupil),
+                        (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
+            # if cv2.waitKey(1) == 27:
+            #     break
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path>')
-def web(path):
-    if(path == 'first' or path == 'second'):
-        return render_template('index.html')
-    elif(path == 'host'):
-        return render_template('host.html')
-    elif(path == 'guest'):
-        return render_template('guest.html')
-    else:
-        return "Hello Page"
+            output_path = os.path.join(output_dir, str(uuid.uuid4()) + ".jpg")
+            cv2.imwrite(output_path, frame)
+            # return json.dumps({"cheated": True, "output_path": output_path})
+            return json.dumps({"nums_of_hand": len(results), "output_path": output_path})
+        
+        # return json.dumps({"nums_of_hand": 0})
+        return json.dumps({"hand_and_eye": 0})
+
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path>')
+# def web(path):
+#     if(path == 'first' or path == 'second'):
+#         return render_template('index.html')
+#     elif(path == 'host'):
+#         return render_template('host.html')
+#     elif(path == 'guest'):
+#         return render_template('guest.html')
+#     else:
+#         return "Hello Page"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", debug=True, ssl_context=(
+    app.run(host="127.0.0.1", port="5000", debug=True, ssl_context=(
         './ssl/localhost.crt', './ssl/localhost.key'))
