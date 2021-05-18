08670835d4a263c1856963d2f5e052001446e92c
lgaeun <gaeungaeun4837@gmail.com>
2021-05-18 16:49:53 +0900

    chat sidebar로 분리

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ import datetime
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def web():    
+def web():
     return render_template('index.html')
 
 
@@ -34,34 +35,35 @@ confidence = 0.25
 output_dir = './output_images'
 os.makedirs(output_dir, exist_ok=True)
 
-# print("loading yolo-tiny-prn...")
-# yolo = YOLO("models/hand/cross-hands-tiny-prn.cfg",
-#             "models/hand/cross-hands-tiny-prn.weights", ["hand"])
-# yolo.size = int(size)
+print("loading yolo-tiny-prn...")
+yolo = YOLO("models/hand/cross-hands-tiny-prn.cfg",
+            "models/hand/cross-hands-tiny-prn.weights", ["hand"])
+yolo.size = int(size)
 
 gaze = GazeTracking()
 
-if network == "normal":
-    print("loading yolo...")
-    yolo = YOLO("models/cross-hands.cfg",
-                "models/cross-hands.weights", ["hand"])
-elif network == "prn":
-    print("loading yolo-tiny-prn...")
-    yolo = YOLO("models/cross-hands-tiny-prn.cfg",
-                "models/cross-hands-tiny-prn.weights", ["hand"])
-elif network == "v4-tiny":
-    print("loading yolov4-tiny-prn...")
-    yolo = YOLO("models/cross-hands-yolov4-tiny.cfg",
-                "models/cross-hands-yolov4-tiny.weights", ["hand"])
-else:
-    print("loading yolo-tiny...")
-    yolo = YOLO("models/cross-hands-tiny.cfg",
-                "models/cross-hands-tiny.weights", ["hand"])
+# if network == "normal":
+#     print("loading yolo...")
+#     yolo = YOLO("models/cross-hands.cfg",
+#                 "models/cross-hands.weights", ["hand"])
+# elif network == "prn":
+#     print("loading yolo-tiny-prn...")
+#     yolo = YOLO("models/cross-hands-tiny-prn.cfg",
+#                 "models/cross-hands-tiny-prn.weights", ["hand"])
+# elif network == "v4-tiny":
+#     print("loading yolov4-tiny-prn...")
+#     yolo = YOLO("models/cross-hands-yolov4-tiny.cfg",
+#                 "models/cross-hands-yolov4-tiny.weights", ["hand"])
+# else:
+#     print("loading yolo-tiny...")
+#     yolo = YOLO("models/cross-hands-tiny.cfg",
+#                 "models/cross-hands-tiny.weights", ["hand"])
 
 yolo.confidence = float(confidence)
 
-### 추가
+# 추가
 # engine = create_engine('mssql+pymssql://username:passwd@host/database', echo=True)
+
 
 @app.route('/api/detection/', methods=['GET', 'POST'])
 def detection():
@@ -77,12 +79,13 @@ def detection():
 
         now = datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S")
 
-        cheat = 0 # 손 또는 눈 detect 여부
+        cheat = 0  # 손 또는 눈 detect 여부
         stid = userId[:7]
-        device = userId[8:] # 핸드폰인지 노트북인지 판별
+        device = userId[8:]  # 핸드폰인지 노트북인지 판별
         print(device)
 
-        output_path = os.path.join(output_dir, str(userId)  + str(now) + str(uuid.uuid4()) + ".jpg")
+        output_path = os.path.join(output_dir, str(
+            userId) + str(now) + str(uuid.uuid4()) + ".jpg")
         #+ ".hand."
 
         ### 손 detect ###
@@ -90,7 +93,7 @@ def detection():
             width, height, inference_time, results = yolo.inference(frame)
 
             print("%s seconds: %s classes found!" %
-                (round(inference_time, 2), len(results)))
+                  (round(inference_time, 2), len(results)))
 
             # 여기에 사진 저장(값 0이면 캡쳐)
             if len(results) < 2:
@@ -108,31 +111,27 @@ def detection():
                     cv2.putText(frame, stid + text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
                                 0.25, color, 1)
 
-                    print("%s with %s confidence" % (name, round(confidence, 2)))
-                    
-                cv2.putText(frame, stid, (90, 60),
-                    cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+                    print("%s with %s confidence" %
+                          (name, round(confidence, 2)))
 
-                
+                cv2.putText(frame, stid, (90, 60),
+                            cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+
                 cv2.imwrite(output_path, frame)
                 return json.dumps({"cheat": 1, "output_path": output_path})
 
-            return json.dumps({"cheat": 0, "output_path": output_path})    
-            
-        
-        
-        ### gaze_Tracking ###
-        
+            return json.dumps({"cheat": 0, "output_path": output_path})
 
-        elif((device == "COM") | (device == "com")) : # 노트북 화면일 경우 
+        ### gaze_Tracking ###
+
+        elif((device == "COM") | (device == "com")):  # 노트북 화면일 경우
             # frame = mat
             eye_text = ""
 
             # We send this frame to GazeTracking to analyze it
             gaze.refresh(frame)
 
-            frame = gaze.annotated_frame() # 동공 십자가 표시
-            
+            frame = gaze.annotated_frame()  # 동공 십자가 표시
 
             if gaze.is_right():
                 cheat = True
@@ -140,8 +139,8 @@ def detection():
                 eye_text = "right"
             elif gaze.is_left():
                 cheat = True
-                # eye_text = "Looking left"   
-                eye_text = "left"     
+                # eye_text = "Looking left"
+                eye_text = "left"
             elif gaze.is_up():
                 cheat = True
                 # eye_text = "Looking up"
@@ -150,11 +149,11 @@ def detection():
                 cheat = False
                 # eye_text = "Looking center"
                 eye_text = "center"
-            else :
+            else:
                 cheat = True
 
             cv2.putText(frame, stid + " - " + eye_text, (90, 60),
-                    cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+                        cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
 
             if cheat:
                 # left_pupil = gaze.pupil_left_coords()
@@ -163,17 +162,17 @@ def detection():
                 #         (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
                 # cv2.putText(frame, "Right pupil: " + str(right_pupil),
                 #         (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-            
+
                 cv2.imwrite(output_path, frame)
                 return json.dumps({"cheat": 1, "output_path": output_path})
-            
+
             else:
                 return json.dumps({"cheat": 0, "output_path": output_path})
 
         else:
             print("id를 '학번_PHONE/COM' 형식으로 입력하지 않았습니다!")
             cv2.putText(frame, stid, (90, 60),
-                    cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+                        cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
             cv2.imwrite(output_path, frame)
             return json.dumps({"cheat": 1, "output_path": output_path})
 
@@ -189,7 +188,7 @@ def detection():
         #                     0.25, color, 1)
 
         #         print("%s with %s confidence" % (name, round(confidence, 2)))
-        
+
         #     cv2.putText(frame, userId +" " + eye_text, (90, 60),
         #             cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)  # 눈 text쓰기
 
@@ -205,11 +204,10 @@ def detection():
 
         #     output_path = os.path.join(output_dir, str(uuid.uuid4()) + ".jpg")
         #     # cv2.imwrite(output_path, frame)
-            
-        #     return json.dumps({"cheat": 1, "output_path": output_path})
-            
 
-        # return json.dumps({"cheat": 1})        
+        #     return json.dumps({"cheat": 1, "output_path": output_path})
+
+        # return json.dumps({"cheat": 1})
 
 
 # @app.route('/', defaults={'path': ''})
