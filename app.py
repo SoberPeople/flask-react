ebd8c2c824063779286c73b6530fad7daaea08a0
dev0529 <xyzkim0529@gmail.com>
2021-06-09 13:55:41 +0900

    image

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import ssl
 import cv2
 from yolo import YOLO
 import json
-import uuid
+# import uuid
 import os
 import base64
 import numpy as np
 from gaze_tracking import GazeTracking
+import datetime
+# from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
@@ -26,17 +28,20 @@ def host():
 def guest():
     return render_template('guest.html')
 
+@app.route('/test/image')
+def image():
+    images = os.listdir('./static/output_images')
+    # images = os.listdir('./output_images')
+    print(images)
+    return render_template('image.html', pics = images)
+
 
 network = 'normal'
 size = 416
 confidence = 0.25
-output_dir = './output_images'
+output_dir = './static/output_images'
 os.makedirs(output_dir, exist_ok=True)
 
-# print("loading yolo-tiny-prn...")
-# yolo = YOLO("models/hand/cross-hands-tiny-prn.cfg",
-#             "models/hand/cross-hands-tiny-prn.weights", ["hand"])
-# yolo.size = int(size)
 
 gaze = GazeTracking()
 
@@ -59,102 +64,130 @@ else:
 
 yolo.confidence = float(confidence)
 
+# 추가
+# engine = create_engine('mssql+pymssql://username:passwd@host/database', echo=True)
+
 
 @app.route('/api/detection/', methods=['GET', 'POST'])
 def detection():
     frame = request.form.get('file')
-    # opencv에서 읽기 위해 8비트 애들을 아스키로 변환
-    img_data = np.frombuffer(base64.b64decode(
-        frame.replace('data:image/png;base64,', '')), np.uint8)
+
+    # 이미지 받아오는 형식에 따라 cv에서 읽을 수 있는 데이터로 변환
+    img_data = ""
+    if "image/jpeg" in frame:
+        img_data = np.frombuffer(base64.b64decode(frame.replace('data:image/jpeg;base64,', '')), np.uint8)
+    else:
+        img_data = np.frombuffer(base64.b64decode(frame.replace('data:image/png;base64,', '')), np.uint8)
     frame = cv2.imdecode(img_data, cv2.IMREAD_ANYCOLOR)
 
     if (request.method == 'POST'):
         userId = request.form.get('id')
-        print(userId)
+        now = datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S")
 
-        cheat = False # 손 또는 눈 detect 여부
+        cheat = 0  # 부정행위이면 1, 아니면 0
+        stid = userId[:7]
+        device = userId[8:]
+        print(now+": "+userId+"의 "+device+"이미지를 받았습니다.")
 
-        # 손 detect
-        width, height, inference_time, results = yolo.inference(frame)
+        output_path = os.path.join(output_dir, str(
+            userId) + str(now) + ".jpg")
+        #+ ".hand."
 
-        print("%s seconds: %s classes found!" %
-              (round(inference_time, 2), len(results)))
+        ### 손 detect ###
+        if((device == "PHONE") | (device == "phone")):
+            width, height, inference_time, results = yolo.inference(frame)
 
-        # 손 0/1개 탐지
-        if len(results) < 2:
-            cheat = True
+            print("%s seconds: %s classes found!" %
+                  (round(inference_time, 2), len(results)))
+
+            # 여기에 사진 저장(값 0이면 캡쳐)
+            if len(results) < 2:
+                cheat = 1
+                # return json.dumps({"cheat": 1})
+
+            if cheat:
+                for detection in results:
+                    id, name, confidence, x, y, w, h = detection
+
+                    # draw a bounding box rectangle and label on the image
+                    color = (255, 0, 255)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), color, 1)
+                    text = "%s (%s)" % (name, round(confidence, 2))
+                    cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                                0.25, color, 1)
+
+                    print("%s with %s confidence" %
+                          (name, round(confidence, 2)))
+
+                cv2.putText(frame, stid, (20, 60),
+                            cv2.FONT_HERSHEY_DUPLEX, 1.6, (0, 80, 0), 2)
+                #147, 58, 31
+
+                if (os.path.isfile(output_path)) :
+                    output_path = os.path.join(output_dir, str(userId) + str(now) + "_1.jpg")
+                
+                cv2.imwrite(output_path, frame)
+                return json.dumps({"cheat": 1, "isHand":1, "numHands":len(results)})
+
+            return json.dumps({"cheat": 0, "isHand":1, "numHands":len(results)})
 
         ### gaze_Tracking ###
-        # frame = mat
-
-        # We send this frame to GazeTracking to analyze it
-        gaze.refresh(frame)
-
-        frame = gaze.annotated_frame() # 동공 십자가 표시
-        eye_text = ""
-
-        if gaze.is_right():
-            cheat = True
-            eye_text = "Looking right"
-        elif gaze.is_left():
-            cheat = True
-            eye_text = "Looking left"        
-        elif gaze.is_up():
-            cheat = True
-            eye_text = "Looking up"
-        elif gaze.is_center():
-            cheat = False
-            eye_text = "Looking center"
-        else :
-            cheat = True
-
-        if cheat: #손 또는 눈이 안 보일 때
-            for detection in results: #손 detect가 있으면 그림그리기
-                id, name, confidence, x, y, w, h = detection
-
-                # draw a bounding box rectangle and label on the image
-                color = (255, 0, 255)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 1)
-                text = "%s (%s)" % (name, round(confidence, 2))
-                cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
-                            0.25, color, 1)
-
-                print("%s with %s confidence" % (name, round(confidence, 2)))
         
-            cv2.putText(frame, userId + eye_text, (90, 60),
-                    cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)  # 눈 text쓰기
+        elif((device == "COM") | (device == "com")):  # 노트북 화면일 경우
+            eye_text = ""
+           
+            gaze.refresh(frame)     # gaze tracking에 분석위해 frame 보내기
+            frame = gaze.annotated_frame()  # 동공 십자가 표시
 
-            left_pupil = gaze.pupil_left_coords()
-            right_pupil = gaze.pupil_right_coords()
-            cv2.putText(frame, "Left pupil:  " + str(left_pupil),
-                        (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-            cv2.putText(frame, "Right pupil: " + str(right_pupil),
-                        (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+            if gaze.is_right():
+                cheat = True
+                eye_text = "right"
+                # print(eye_text + "  horizontal: " + str(round(gaze.horizontal_ratio(),2))
+                #  + "  vertical: " + str(round(gaze.vertical_ratio(),2)))
+            elif gaze.is_left():
+                cheat = True
+                eye_text = "left"
+                # print(eye_text + "  horizontal: " + str(round(gaze.horizontal_ratio(),2))
+                #  + "  vertical: " + str(round(gaze.vertical_ratio(),2)))
+            elif gaze.is_up():
+                cheat = True
+                eye_text = "up"
+                # print(eye_text + "  horizontal: " + str(round(gaze.horizontal_ratio(),2))
+                #  + "  vertical: " + str(round(gaze.vertical_ratio(),2)))
+            elif gaze.is_center():
+                cheat = False
+                eye_text = "center"
+            else:
+                cheat = False
 
-            # if cv2.waitKey(1) == 27:
-            #     break
+            cv2.putText(frame, stid + " - " + eye_text, (90, 60),
+                        cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
 
-            output_path = os.path.join(output_dir, str(uuid.uuid4()) + ".jpg")
+            if cheat:
+                # left_pupil = gaze.pupil_left_coords()
+                # right_pupil = gaze.pupil_right_coords()
+                # cv2.putText(frame, "Left pupil:  " + str(left_pupil),
+                #         (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+                # cv2.putText(frame, "Right pupil: " + str(right_pupil),
+                #         (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+
+                cv2.imwrite(output_path, frame)
+                return json.dumps({"cheat": 1, "isHand":0, "dir": eye_text})
+
+            else:
+                return json.dumps({"cheat": 0, "isHand":0, "dir": eye_text})
+
+        else:
+            print("id를 '학번_PHONE/COM' 형식으로 입력하지 않았습니다!")
+
+            cv2.putText(frame, stid + "  ID Error", (20, 60),
+                        cv2.FONT_HERSHEY_DUPLEX, 1.6, (0, 80, 0), 2)
+
             cv2.imwrite(output_path, frame)
-            # return json.dumps({"cheated": True, "output_path": output_path})
-            return json.dumps({"nums_of_hand": len(results), "output_path": output_path})
-        
-        # return json.dumps({"nums_of_hand": 0})
-        return json.dumps({"hand_and_eye": 0})
+            return json.dumps({"cheat": 2})
 
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path>')
-# def web(path):
-#     if(path == 'first' or path == 'second'):
-#         return render_template('index.html')
-#     elif(path == 'host'):
-#         return render_template('host.html')
-#     elif(path == 'guest'):
-#         return render_template('guest.html')
-#     else:
-#         return "Hello Page"
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port="5000", debug=True, ssl_context=(
+    app.run(host="0.0.0.0", port="5000", debug=True, ssl_context=(
         './ssl/localhost.crt', './ssl/localhost.key'))
--- gaze_tracking/gaze_tracking.py
+++ gaze_tracking/gaze_tracking.py
@@ -24,7 +24,8 @@ class GazeTracking(object):
 
         # _predictor is used to get facial landmarks of a given face
         cwd = os.path.abspath(os.path.dirname(__file__))
-        model_path = os.path.abspath(os.path.join(cwd, "trained_models/shape_predictor_68_face_landmarks.dat"))
+        model_path = os.path.abspath(os.path.join(
+            cwd, "trained_models/shape_predictor_68_face_landmarks.dat"))
         self._predictor = dlib.shape_predictor(model_path)
 
     @property
@@ -49,13 +50,13 @@ class GazeTracking(object):
             self.eye_left = Eye(frame, landmarks, 0, self.calibration)
             self.eye_right = Eye(frame, landmarks, 1, self.calibration)
 
+
         except IndexError:
             self.eye_left = None
             self.eye_right = None
 
     def refresh(self, frame):
         """Refreshes the frame and analyzes it.
-
         Arguments:
             frame (numpy.ndarray): The frame to analyze
         """
@@ -82,8 +83,10 @@ class GazeTracking(object):
         the center is 0.5 and the extreme left is 1.0
         """
         if self.pupils_located:
-            pupil_left = self.eye_left.pupil.x / (self.eye_left.center[0] * 2 - 10)
-            pupil_right = self.eye_right.pupil.x / (self.eye_right.center[0] * 2 - 10)
+            pupil_left = self.eye_left.pupil.x / \
+                (self.eye_left.center[0] * 2 - 10)
+            pupil_right = self.eye_right.pupil.x / \
+                (self.eye_right.center[0] * 2 - 10)
             return (pupil_left + pupil_right) / 2
 
     def vertical_ratio(self):
@@ -92,41 +95,69 @@ class GazeTracking(object):
         the center is 0.5 and the extreme bottom is 1.0
         """
         if self.pupils_located:
-            pupil_left = self.eye_left.pupil.y / (self.eye_left.center[1] * 2 - 10)
-            pupil_right = self.eye_right.pupil.y / (self.eye_right.center[1] * 2 - 10)
+            pupil_left = self.eye_left.pupil.y / \
+                (self.eye_left.center[1] * 2 - 10)
+            pupil_right = self.eye_right.pupil.y / \
+                (self.eye_right.center[1] * 2 - 10)
             return (pupil_left + pupil_right) / 2
 
     def is_right(self):
         """Returns true if the user is looking to the right"""
+        # if self.pupils_located:
+        #     return self.horizontal_ratio() <= 0.35
         if self.pupils_located:
-            return self.horizontal_ratio() <= 0.35 and self.vertical<0.6
+            if self.horizontal_ratio() <= 0.45:
+                return True
 
     def is_left(self):
         """Returns true if the user is looking to the left"""
+        # if self.pupils_located:
+        #     return self.horizontal_ratio() >= 0.65 # and self.vertical_ratio >= 0.35
         if self.pupils_located:
-            return self.horizontal_ratio() >= 0.65
+            if self.horizontal_ratio() >= 0.7:
+                return True
 
     def is_center(self):
         """Returns true if the user is looking to the center"""
+        # if self.pupils_located:
+        #     return self.is_right() is not True and self.is_left() is not True and self.is_up() is not True and self.is_down() is not True
         if self.pupils_located:
-            return self.is_right() is not True and self.is_left() is not True and self.is_up() is not True and self.is_down() is not True
-            
+            if self.is_right() is not True and self.is_left() is not True and self.is_up() is not True and self.is_down() is not True:
+                return True
+
     def is_up(self):
         """Returns true if the user is looking to the up"""
+        # if self.pupils_located:
+        #     return self.vertical_ratio() <= 0.65
         if self.pupils_located:
-            return self.vertical_ratio() <= 0.35
+            if self.vertical_ratio() <= 0.5 and self.is_right() is not True and self.is_left() is not True:
+                return True
 
     def is_down(self):
         """Returns true if the user is looking to the down"""
+        # if self.pupils_located:
+        #     return self.vertical_ratio() >= 0.65
         if self.pupils_located:
-            return self.vertical_ratio() >= 0.65            
-        
+            if self.vertical_ratio() >= 0.65 and self.is_right() is not True and self.is_left() is not True:
+                return True
 
-    def is_blinking(self):
+    # def is_blinking(self):
+    #     """Returns true if the user closes his eyes"""
+    #     if self.pupils_located:
+    #         blinking_ratio = (self.eye_left.blinking + self.eye_right.blinking) / 2
+    #         return blinking_ratio > 3.8
+    
+    def head_direction(self):
         """Returns true if the user closes his eyes"""
-        if self.pupils_located:
-            blinking_ratio = (self.eye_left.blinking + self.eye_right.blinking) / 2
-            return blinking_ratio > 3.8
+        left_ear=landmarks.part(2)
+        right_ear=landmarks.part(16)
+        nose=landmarks.part(30)
+        head_ratio= abs(left_ear-nose)/abs(right_ear-nose)
+
+        if head_ratio > 1:
+            return right
+        if head_ration < 1:
+            return left
 
     def annotated_frame(self):
         """Returns the main frame with pupils highlighted"""
@@ -138,7 +169,9 @@ class GazeTracking(object):
             x_right, y_right = self.pupil_right_coords()
             cv2.line(frame, (x_left - 5, y_left), (x_left + 5, y_left), color)
             cv2.line(frame, (x_left, y_left - 5), (x_left, y_left + 5), color)
-            cv2.line(frame, (x_right - 5, y_right), (x_right + 5, y_right), color)
-            cv2.line(frame, (x_right, y_right - 5), (x_right, y_right + 5), color)
+            cv2.line(frame, (x_right - 5, y_right),
+                     (x_right + 5, y_right), color)
+            cv2.line(frame, (x_right, y_right - 5),
+                     (x_right, y_right + 5), color)
 
         return frame
